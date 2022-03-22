cd16578949ac4e7a1f30c3c4214f0f4906bbf104
joaocarvalho19 <joao.carvalho19@hotmail.com>
2022-03-22 16:48:04 +0000

    netbeans project

---
+++ PA1C2GY/src/main/java/com/mycompany/pa1c2gy/PA1C2GY.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pa1c2gy;
+
+/**
+ *
+ * @author joaoc
+ */
+public class PA1C2GY {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
